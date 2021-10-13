9193edb1db715ad8f5e2144590cf3aea85e8f867
guiller91 <iphoneguiller91@gmail.com>
2021-10-13 19:41:30 +0200

    quitados comentarios sobrantes

--- Actividad_1/src/requerimiento_1/PrimeChecker.java
+++ Actividad_1/src/requerimiento_1/PrimeChecker.java
@@ -13,22 +13,16 @@ public class PrimeChecker extends Thread {
 
 	
 	public void run() {
-		System.out.println("Se esta iniciando el hilo: " + Thread.currentThread().getName());
-		
-		
+		System.out.println("Se esta iniciando el hilo: " + Thread.currentThread().getName());		
 		// iniciamos una variable para saber los mili segundos
-		// lo coloco aqui para que cuente realmente cuanto tarda el hilo en procesar el problema
 		inicio = System.currentTimeMillis();
 		//Nos dira el hilo  actual y si es primo o no.
 		System.out.println("El hilo : " + Thread.currentThread().getName() + " determina que " + primeNumber(numero));
 		// iniciamos otra variable que nos dira en que nano segundo estamos y nos la restara de la variable inicio para saber lo que tarda.
-
 		fin = System.currentTimeMillis();
 		tiempo = (fin - inicio)/1000;
 		// tiempo que tarda en hilo en hacer la operación
-
 		System.out.println("El "+ Thread.currentThread().getName() + " ha tardado en procesar la informacion " + tiempo + " nano segundos");
-
 	}
 
 	public static String primeNumber(long num) {
