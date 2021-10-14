fc2705c472be4e0a11534270ceb0e39eae6fd17e
guiller91 <iphoneguiller91@gmail.com>
2021-10-14 13:06:18 +0200

    añadido metodo calculador de tiempo en funcion de lo que tarda

--- Actividad_1/src/requerimiento_1/PrimeChecker.java
+++ Actividad_1/src/requerimiento_1/PrimeChecker.java
@@ -5,7 +5,7 @@ public class PrimeChecker extends Thread {
 	private long numero;
 	private long inicio;
 	private long fin;
-	private long tiempo;
+	
 
 	public PrimeChecker(long numero) {
 		this.numero = numero;
@@ -19,10 +19,9 @@ public class PrimeChecker extends Thread {
 		//Nos dira el hilo  actual y si es primo o no.
 		System.out.println("El hilo : " + Thread.currentThread().getName() + " determina que " + primeNumber(numero));
 		// iniciamos otra variable que nos dira en que mili segundo estamos y nos la restara de la variable inicio para saber lo que tarda.
-		fin = System.currentTimeMillis();
-		tiempo = (fin - inicio)/1000;
+		fin = System.currentTimeMillis();		
 		// tiempo que tarda en hilo en hacer la operación
-		System.out.println("El "+ Thread.currentThread().getName() + " ha tardado en procesar la informacion " + tiempo + " segundos");
+		System.out.println(calculadorTiempo(inicio,fin));;
 	}
 
 	public static String primeNumber(long num) {
@@ -37,5 +36,16 @@ public class PrimeChecker extends Thread {
 			return "El número " + num + " es primo.";
 		else
 			return "El número " + num + " no es primo.";
+	}
+	
+	// Nos dira, dependiendo del tiempo tardado, su duración o en mili segundos ( si no llega al minimo de 1 segundo) o en segundos.
+	public static String calculadorTiempo(long inicio, long fin) {
+		long tiempo = fin - inicio;		
+		if(tiempo/1000 == 0) {
+			return "El "+ Thread.currentThread().getName() + " ha tardado en procesar la informacion " + tiempo + " mili segundos";
+		}
+		else {
+			return "El "+ Thread.currentThread().getName() + " ha tardado en procesar la informacion " + (tiempo/1000) + " segundos";
+		}
 	}
 }
